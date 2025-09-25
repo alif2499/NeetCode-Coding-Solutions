a4638f2ca488520eb423cbadeea6c3d50b440041
alif2499 <alifjcc99@gmail.com>
2025-09-25 14:48:30 -0600

    Solved Longest Consecutive Sequence with O(n)

--- Arrays & Hashing/Longest Consecutive Sequence.py
+++ Arrays & Hashing/Longest Consecutive Sequence.py
@@ -1,5 +1,5 @@
 class Solution:
-    # solution with nlogn time complexity (depending on sorting algorithm) 
+    # solution with O(nlogn) time complexity (depending on sorting algorithm) 
     def longestConsecutive(self, nums: List[int]) -> int:
         seq = 1
         longest = 0
@@ -16,3 +16,21 @@ class Solution:
                     longest = seq
                 seq = 1
         return longest
+    
+    # solution with O(n) time complexity. Identifying the starting of a sequence
+    def longestConsecutive(self, nums: List[int]) -> int:
+        store = set(nums)
+        seqList = []
+        maxi = 0
+        for i in store:
+            seq = []
+            if i-1 not in store:
+                seq.append(i)
+                while i+1 in store:
+                    seq.append(i+1)
+                    i = i+1
+                seqList.append(seq)
+        for i in range(len(seqList)):
+            if len(seqList[i])>maxi:
+                maxi = len(seqList[i])
+        return maxi
