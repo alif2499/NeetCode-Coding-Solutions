ab13067985b43d7354a67cbb907ff0641d8c6334
alif2499 <alifjcc99@gmail.com>
2025-10-14 10:35:49 -0600

    Solved Search in Rotated Sorted Array

--- Binary Search/Search in Rotated Sorted Array.py
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -6,5 +6,14 @@ class Solution:
             mid = (l + r) // 2
             if target == nums[mid]:
                 return mid
-
-            
+            elif nums[l] <= nums[mid]:
+                if target > nums[mid] or target <nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
