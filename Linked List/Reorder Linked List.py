5aa8c0b01440a68a4500266c1b1478ab3ec916d8
alif2499 <alifjcc99@gmail.com>
2025-10-25 13:25:22 -0600

    Working on Reorder Linked List

--- Linked List/Reorder Linked List.py
+++ Linked List/Reorder Linked List.py
@@ -6,4 +6,16 @@
 
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        i=0
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
